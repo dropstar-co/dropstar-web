4da366110b29701326d8b38f9a3998bcb65ed466
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-03-10 15:25:18 +0100

    Venly integration, first approach
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,26 @@
-import "./Header.css";
+import './Header.css';
 
-import { NavLink, useHistory } from "react-router-dom";
-import React, { useState } from "react";
-import { getUserAuthState, getUserProfile } from "../../store/selectors/user";
-import { setUserAuthState, setUserProfile } from "../../store/actions/user";
-import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { getUserAuthState, getUserProfile } from '../../store/selectors/user';
+import { setUserAuthState, setUserProfile } from '../../store/actions/user';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Avatar from "../../assets/svg/user.svg";
-import { Dropdown } from "react-bootstrap";
-import LoginButton from "../buttons/login/LoginButton";
-import Logo from "../../assets/svg/logo.svg";
-import MenuDropdown from "../menu-dropdown/MenuDropdown";
-import { fetchLoggedInUser } from "../../store/actions/user";
-import venlyHelpers from "../../helpers/venly";
+import Avatar from '../../assets/svg/user.svg';
+import { Dropdown } from 'react-bootstrap';
+import LoginButton from '../buttons/login/LoginButton';
+import Logo from '../../assets/svg/logo.svg';
+import MenuDropdown from '../menu-dropdown/MenuDropdown';
+import { fetchLoggedInUser } from '../../store/actions/user';
+import venlyHelpers from '../../helpers/venly';
+
+import { VenlyConnect } from '@venly/connect';
+const VENLY_WIDGET_CLIENT_ID = 'Testaccount';
+const VENLY_CHAIN = 'MATIC';
+const VENLY_ENVIRONMENT = 'staging';
+const venlyConnect = new VenlyConnect(VENLY_WIDGET_CLIENT_ID, {
+  environment: VENLY_ENVIRONMENT,
+});
 
 const Header = () => {
   const history = useHistory();
@@ -20,8 +28,66 @@ const Header = () => {
   const isUserAuthenticated = useSelector(getUserAuthState);
   const profile = useSelector(getUserProfile);
 
+  async function checkAuthenticated() {
+    const result = await venlyConnect.checkAuthenticated();
+
+    console.log({ result });
+
+    const { email, userId } = result;
+    const filteredResult = { email, userId };
+    console.log(JSON.stringify({ filteredResult }, null, 2));
+    result.authenticated(async function (auth) {
+      dispatch(setUserAuthState(true));
+    });
+  }
+
+  function init() {
+    checkAuthenticated();
+  }
+
+  async function connect(venlyConnect) {
+    try {
+      const account = await venlyConnect.flows.getAccount(VENLY_CHAIN);
+      console.log({ account });
+      dispatch(setUserProfile(Object.assign(profile, { wallets: account.wallets })));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function updateData() {
+    const ve = await venlyConnect.api.getProfile();
+
+    console.log('ve.email');
+    console.log(ve.email);
+    console.log('ve.userId');
+    console.log(ve.userId);
+
+    dispatch(fetchLoggedInUser({ Email: ve?.email, VenlyUID: ve?.userId }));
+    dispatch(
+      setUserProfile({
+        userId: ve?.userId,
+        email: ve?.email,
+        firstName: ve?.firstName,
+        lastName: ve?.lastName,
+        hasMasterPin: ve?.hasMasterPin,
+      }),
+    );
+
+    const wallets = await venlyConnect.api.getWallets({ secretType: VENLY_CHAIN });
+    dispatch(setUserProfile(Object.assign(profile, { wallets })));
+  }
+
+  useEffect(init, []);
+  useEffect(() => {
+    if (isUserAuthenticated) updateData();
+  }, [isUserAuthenticated]);
+
   const handleLogin = async () => {
+    console.log('Doing the logging');
     const ve = await venlyHelpers.login();
+
+    console.log('ALready logged');
     if (ve.userId && ve?.email) {
       dispatch(fetchLoggedInUser({ Email: ve?.email, VenlyUID: ve?.userId }));
       dispatch(setUserAuthState(true));
@@ -32,7 +98,7 @@ const Header = () => {
           firstName: ve?.firstName,
           lastName: ve?.lastName,
           hasMasterPin: ve?.hasMasterPin,
-        })
+        }),
       );
     }
   };
@@ -43,7 +109,7 @@ const Header = () => {
     console.log(showMenu);
     setShowMenu(!showMenu);
   };
-  const DirectToDiscoverPage = () => history.push("/discover");
+  const DirectToDiscoverPage = () => history.push('/discover');
 
   return (
     <>
@@ -63,18 +129,13 @@ const Header = () => {
             {isUserAuthenticated && (
               <Dropdown>
                 <Dropdown.Toggle id="dropdown-custom-1">
-                  <img
-                    src={Avatar}
-                    alt="user"
-                    className="user-image"
-                    onClick={ToggleShowMenu}
-                  />
+                  <img src={Avatar} alt="user" className="user-image" onClick={ToggleShowMenu} />
                 </Dropdown.Toggle>
                 <MenuDropdown />
               </Dropdown>
             )}
             {!isUserAuthenticated && (
-              <LoginButton handleClick={handleLogin} text="Login" />
+              <LoginButton handleClick={() => connect(venlyConnect)} text="Login" />
             )}
           </div>
         </div>
--- src/helpers/venly.js
+++ src/helpers/venly.js
@@ -1,39 +1,53 @@
-import { VenlyConnect, WindowMode } from "@venly/connect";
+import { VenlyConnect, WindowMode } from '@venly/connect';
 import axios from 'axios';
+
+const VENLY_WIDGET_CLIENT_ID = 'Testaccount';
+const VENLY_CHAIN = 'MATIC';
+const VENLY_ENVIRONMENT = 'staging';
+const venlyConnect = new VenlyConnect(VENLY_WIDGET_CLIENT_ID, {
+  environment: VENLY_ENVIRONMENT,
+});
+
 class venlyHelpers {
-  static async login() {
-    const venlyConnect = new VenlyConnect("Testaccount", {
-      environment: "staging",
+  static async checkAuthenticated() {
+    const result = await venlyConnect.checkAuthenticated();
+    result.authenticated(async function (auth) {
+      const wallets = await venlyConnect.api.getWallets({ secretType: VENLY_CHAIN });
     });
+  }
+
+  static init() {
+    venlyHelpers.checkAuthenticated();
+  }
+
+  static async connect(venlyConnect) {
+    try {
+      const account = await venlyConnect.flows.getAccount(VENLY_CHAIN);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async login() {
     const loginObject = await venlyConnect.flows.authenticate();
     const wallets = await venlyConnect.api.getProfile();
     return wallets;
   }
 
   static async loadProfile() {
-    const venlyConnect = new VenlyConnect("Testaccount", {
-      environment: "staging",
-    });
     await venlyConnect.flows.authenticate();
     const wallets = await venlyConnect.api.getProfile();
     return wallets;
   }
 
   static async checkAuth() {
-
-    const venlyConnect = new VenlyConnect("Testaccount", {
-      environment: "staging",
-    });
     const checkingauth = await venlyConnect.checkAuthenticated();
     return checkingauth;
   }
   static async logOut() {
-    const venlyConnect = new VenlyConnect("Testaccount", {
-      environment: "staging",
-    });
-    localStorage.removeItem('dstoken')
-    const  authObj = venlyConnect.checkAuthenticated()
-    await (await authObj).auth.clearToken()
+    localStorage.removeItem('dstoken');
+    const authObj = venlyConnect.checkAuthenticated();
+    await (await authObj).auth.clearToken();
     await venlyConnect.logout({ windowMode: 'REDIRECT' });
     return await venlyConnect.logout();
   }
