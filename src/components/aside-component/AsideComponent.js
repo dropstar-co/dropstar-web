4de1e01a84efd51f46155b23811ba3e164e9a344
Jorge Castro-Gallardo <jcastro.inf@gmail.com>
2022-04-07 12:34:58 +0200

    Add auction state in discover page
    Signed-off-by: Jorge Castro-Gallardo <jcastro.inf@gmail.com>

--- src/components/aside-component/AsideComponent.js
+++ src/components/aside-component/AsideComponent.js
@@ -1,32 +1,48 @@
-import React from "react";
-import { Accordion } from "react-bootstrap";
-import "./AsideComponent.css";
-import { setCurrentArtist } from "../../store/actions/discover";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { Accordion } from 'react-bootstrap';
+import './AsideComponent.css';
+import { setCurrentArtist } from '../../store/actions/discover';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import humanizeDuration from 'humanize-duration';
 
 const AsideComponent = ({ title, data }) => {
   const dispatch = useDispatch();
   const history = useHistory();
+
+  console.log({ hd: humanizeDuration(32846000) });
   return (
     <div className="aside-page">
       <h4 className="title">{title}</h4>
       <Accordion flush>
-        {data?.map((datum) => (
+        {data?.map(datum => (
           <Accordion.Item
             eventKey={datum.id}
             key={datum.id}
-            onClick={() => dispatch(setCurrentArtist(datum))}
-          >
+            onClick={() => dispatch(setCurrentArtist(datum))}>
             <Accordion.Header className="accord-header">
-              {datum.name}
+              {Date.parse(datum.EndDate) < Date.now() ? (
+                <div>
+                  <div>{`${datum.name}  `}</div>
+                  <div style={{ color: 'red' }}>Sold</div>
+                </div>
+              ) : Date.parse(datum.EndDate) - Date.now() > 60 * 60 * 24 ? (
+                <div>
+                  <div>{`${datum.name}  `}</div>
+                  <div style={{ color: 'green' }}>
+                    {humanizeDuration(Date.parse(datum.EndDate) - Date.now(), { largest: 2 })}
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div>{`${datum.name}  `}</div>
+                  <div style={{ color: 'green' }}>Auction is live</div>
+                </div>
+              )}
             </Accordion.Header>
             <Accordion.Body>
               <div className="content">{datum.NftAbout}</div>
-              <div
-                className="show-detail-text"
-                onClick={() => history.push(`/nfts/${datum.id}`)}
-              >
+              <div className="show-detail-text" onClick={() => history.push(`/nfts/${datum.id}`)}>
                 Show Details
               </div>
             </Accordion.Body>
